9c6a47ff8e1839784dd3b88e93552a4fd5af12d2
Rizky_Yudha <102364151+Rzydx@users.noreply.github.com>
2022-09-09 00:03:50 +0700

    Delete config.py

--- config.py
+++
@@ -1,5 +0,0 @@
-from os import environ
-
-class Config(object):
-#The Telegram channel id you want focus user.(User can't start your bot without join it)
-        F_SUB_CHANNEL = environ.get("F_SUB_CHANNEL")
