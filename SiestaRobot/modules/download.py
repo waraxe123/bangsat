1b4b783ad9d7177a25bf30b0ba22090271f6c8c8
Rizky_Yudha <102364151+Rzydx@users.noreply.github.com>
2022-09-08 20:35:43 +0700

    Create download.py

---
+++ SiestaRobot/modules/download.py
@@ -0,0 +1,31 @@
+from telethon.errors.rpcerrorlist import YouBlockedUserError
+from SiestaRobot import bot, CMD_HELP
+from SiestaRobot.events import register
+
+
+@register(outgoing=True, pattern="^/tt(?: |$)(.*)")
+async def _(event):
+    if event.fwd_from:
+        return
+    d_link = event.pattern_match.group(1)
+    if ".com" not in d_link:
+        await event.edit("`Mohon Maaf, Saya Membutuhkan Link Video Tiktok Untuk Mendownload Nya`")
+    else:
+        await event.edit("```Video Sedang Diproses.....```")
+    chat = "@TIKTOKDOWNLOADROBOT"
+    async with bot.conversation(chat) as conv:
+        try:
+            msg_start = await conv.send_message("/start")
+            r = await conv.get_response()
+            msg = await conv.send_message(d_link)
+            details = await conv.get_response()
+            video = await conv.get_response()
+            """ - don't spam notif - """
+            await bot.send_read_acknowledge(conv.chat_id)
+        except YouBlockedUserError:
+            await event.edit("**Kesalahan:** **Mohon Buka Blokir** `@TIKTOKDOWNLOADROBOT` **Dan Coba Lagi !**")
+            return
+        await bot.send_file(event.chat_id, video)
+        await event.client.delete_messages(conv.chat_id,
+                                           [msg_start.id, r.id, msg.id, details.id, video.id])
+        await event.delete()
