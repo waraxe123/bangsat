95957c22deac049463d0176d66c95e39998d700e
Rizky_Yudha <102364151+Rzydx@users.noreply.github.com>
2022-09-16 19:39:57 +0700

    Update sangmata.py

--- SiestaRobot/modules/sangmata.py
+++ SiestaRobot/modules/sangmata.py
@@ -30,7 +30,7 @@ async def lastname(steal):
                 response = await conv.get_response()
             except YouBlockedUserError:
                 await steal.reply(
-                    "```Error, report to @kenbotsupport```"
+                    "```Error, report to @NiskalaSupport```"
                 )
                 return
             if r.text.startswith("Name"):
